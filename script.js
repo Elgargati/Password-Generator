9a38d247431c0c8421010e2895ea7aa56d7c06cb
Mohammed El gargati <mohammedelgargati02@gmail.com>
2024-10-23 12:17:55 +0100

    v1

---
+++ script.js
@@ -0,0 +1,25 @@
+const passwordBox = document.getElementById("password");
+const lenght = 12;
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const toLowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "!@#$%&()*+,-./:;<=>?@[^_|}~";
+
+const allChars = upperCase + toLowerCase + number + symbol;
+console.log(allChars);
+
+function createPassword() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += toLowerCase[Math.floor(Math.random() * toLowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+  password += symbol[Math.floor(Math.random() * symbol.length)];
+  while (lenght > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+  passwordBox.value = password;
+}
+function copyPassword() {
+  passwordBox.select();
+  document.execCommand("copy");
+}
